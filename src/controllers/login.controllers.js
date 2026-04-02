952635581d4a59e0142b6a1fdeb8f30f95a1840f
Kishor Gharti <kishor.gharti95@gmail.com>
2026-04-02 12:34:06 +0545

    Login

---
+++ src/controllers/login.controllers.js
@@ -0,0 +1,40 @@
+import user from "../models/user.models.js";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+
+export const loginController = async (req,res,next) =>{
+    try{
+        const {email,password} = req.body;
+
+        const emailChecker = await user.findOne({email})
+        if(!emailChecker){
+            return res.status(401).json({message:'User not found'})
+        }
+
+        const pass = await bcrypt.compare(password,emailChecker.password)
+        if (!pass){
+            return res.status(401).json({message:'Password wrong'})
+        }
+
+        const token = jwt.sign(
+        { id: user._id, email: user.email },
+        process.env.JWT_SECRET,              
+        { expiresIn: "1h" }                 
+        );
+
+        res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000, 
+        });
+
+       // console.log(token);
+
+        res.status(200).json({message:'Login successfull'})
+    }
+    catch(err){
+        return res.status(503).json({message:'Unable to login'})
+    }
+
+
+}
--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,7 +1,9 @@
 import { registerUser } from "../controllers/auth.controllers.js";
+import { loginController } from "../controllers/login.controllers.js";
 import express from 'express'
 const router = express.Router()
 
 router.post('/register', registerUser);
+router.post('/login',loginController);
 
 export default router;
